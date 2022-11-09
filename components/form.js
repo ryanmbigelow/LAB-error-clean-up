c3d9c811f4aa2ed6994e1c9197a3fd627e6d8a67
ryanmbigelow <ryanmbigelow@gmail.com>
2022-11-09 13:38:04 -0600

    modularized all data

---
+++ components/form.js
@@ -0,0 +1,25 @@
+// Add events for form after the form is on the DOM
+import renderToDOM from '../utils/renderToDom';
+import sortStudent from '../utils/sortStudent';
+
+const form = () => {
+  const domString = `<form id="sorting" class="d-flex flex-column form-floating ">
+    <input
+    type="text"
+    class="form-control mb-1"
+    id="student-name"
+    placeholder="Enter a name"
+    required
+  />
+  <label for="floatingInputValue">Name to be sorted</label>
+<button type="submit" class="btn btn-success">Get Sorted!</button>
+</form>`;
+
+  renderToDOM('#form-container', domString);
+
+  // has to be put on the DOM after form is on DOM, not before
+  // on form submit, sort student
+  document.querySelector('#sorting').addEventListener('submit', sortStudent);
+};
+
+export default form;
---
+++ utils/sortStudent.js
@@ -0,0 +1,26 @@
+// sorts student to a house and then place them in the students array
+import { students, houses } from './arrays';
+import createId from './createId';
+import { studentsOnDom } from '../components/htmlStructure';
+
+const sortStudent = (e) => {
+  e.preventDefault();
+  const sortingHat = houses[Math.floor(Math.random() * houses.length)];
+
+  if (e.target.id === 'sorting') {
+    const student = document.querySelector('#student-name');
+
+    // create the new student object
+    students.push({
+      id: createId(students),
+      name: student.value,
+      house: sortingHat.house,
+      crest: sortingHat.crest
+    });
+
+    student.value = ''; // reset value of input
+    studentsOnDom('#students', students);
+  }
+};
+
+export default sortStudent;
